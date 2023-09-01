1594c789ba5671147abbdceed7563db1a7f96fdc
Petrus Mahardika <petrus.mahardika@gmail.com>
2023-09-02 01:46:03 +1000

    first commit

---
+++ js/main.js
@@ -0,0 +1,463 @@
+/* 
+  Week 7 Tuesday homework:
+
+  Replicate the UI of www.themoviedb.org on your own SPA!
+
+  1. When the user types a search and clicks the button,
+     get the text they typed and use it to perform an AJAX
+     request to the TMDB API, replacing the hardcoded `alien` 
+     in `&query=alien` of the URL with the user's search text.
+     
+  2. Loop through the results and show them on the page - at least
+     the movie title, year, and overview text. Try to work out
+     how to show an appropriate small size of movie poster image too!
+     You might have to RTFM or STFW for this one since you don't get 
+     the full poster URL with the results for each movie. Use some
+     CSS to make it look nice - lay them out in a grid, for example,
+     i.e. not just one movie per row, but across the page (this is
+     only a couple of CSS properties).
+     
+  3. Set up a click handler so that when a user clicks on a movie, 
+     they are "taken" to a movie details "page", which for an SPA
+     actually means that you hide the search results <div> (using HTML 
+     classes and CSS), and show a details <div>. To get the more detailed
+     results for a single movie to show on this page, you will have 
+     to know the id of the specific movie that was clicked, (hints in 
+     the .then() handler below ) and plug it into a *different* endpoint URL 
+     (docs here: https://developer.themoviedb.org/reference/movie-details) 
+     to make an AJAX request to. When you get that data back, format 
+     it into HTML tags and add it to the details <div> of your page.
+     Display whatever details sound interesting - budget & revenue, 
+     production company info, languages, votes/ratings, genre tags, etc.
+     What about a larger poster, or using the `backdrop_path` as your
+     background image, or links to the official website, or the IMDB page? 
+
+  4. OPTIONAL/BONUS: 
+      - Make a 'back' link on the movie details page to "take you back"
+        to the previous search results "page" (which will actually just
+        be the reverse of seeing this details "page", i.e. showing and
+        hiding the relevant <div>s.)
+      - Implement pagination: use the `&page=N` querystring parameter to
+        request pages of results beyond the first, and connect these
+        requests to UI elements such as "Previous Page" and "Next Page"
+        links, or even a list of page number links " << 1 2 3 4 >> "
+      - Use the production companies data in the details to link to
+        a production company "page", which might show other movies
+        by the same company. 
+      - Use the Movie Credits endpoint 
+        (docs: https://developer.themoviedb.org/reference/movie-credits) 
+        to add detailed cast & crew information, including headshots, to 
+        the movie details page (by making a second AJAX request). What
+        about a details page for an actor, listing the movies they have
+        featured in, with links to those movies (i.e. your own movie 
+        details page, from the search results)
+      - Play around and experiment! What other data does this huge API
+        offer that you can integrate and cross-reference with? 
+        What about allowing your users to create favourites/bookmarks
+        of the movies they are browsing? You might want to use the 
+        browser's LocalStorage to persist this data across page loads.
+
+
+  RTFM: https://developer.themoviedb.org/reference/intro/getting-started
+*/
+
+const TMDB_MOVIE_SEARCH_URL = 'https://api.themoviedb.org/3/search/movie';
+const TMDB_API_KEY = '24d863d54c86392e6e1df55b9a328755';
+
+const favouritesButton = document.querySelector('#favouritesButton');
+const searchResultsContainer = document.querySelector('#searchResults');
+const movieDetailsContainer = document.querySelector('#movieDetails');
+const castDetailsContainer = document.querySelector('#castDetails');
+const searchFormNode = document.querySelector('#searchForm');
+const movieQueryInput = document.querySelector('#movieQuery');
+const errorMessageNode = document.querySelector('#errorMessage');
+let favouriteMovieIDs = [];
+
+// Retrieve favourite movies from localStorage
+const savedMovieIDs = JSON.parse(localStorage.getItem('favourites'));
+if(Array.isArray(savedMovieIDs)) {
+  favouriteMovieIDs = savedMovieIDs;
+}
+
+// Handle the form input text
+searchFormNode.addEventListener('submit', function(ev){
+  ev.preventDefault();
+  const newMovieQuery = movieQueryInput.value;
+
+  if(newMovieQuery.trim().length === 0) {
+    errorMessageNode.innerHTML = 'Please enter a movie name.'
+    return; //early return
+  }
+  
+  loadSearchResults(newMovieQuery);
+});
+
+// When user start typing on the input field, the previous error message disappear
+movieQueryInput.addEventListener('input', function(ev){
+  if(ev.target.value.trim().length > 0){
+    errorMessageNode.innerHTML = '';
+  }
+});
+
+// When Favourites button is clicked
+favouritesButton.addEventListener('click', function(ev) {
+  ev.preventDefault();
+
+  if(favouriteMovieIDs.length > 0) {
+    // Clear previous search results
+    searchResultsContainer.replaceChildren();
+    // Make sure movieDetailsContainer and castDetailsContainer are hidden
+    movieDetailsContainer.style.display = 'none';
+    castDetailsContainer.style.display = 'none';
+    favouriteMovieIDs.forEach(id => {
+      axios.get(`https://api.themoviedb.org/3/movie/${id}`, {
+        params: {
+          api_key: TMDB_API_KEY
+        }
+      })
+      .then(res => {
+        generateFavouriteMovie(res);
+      })
+      .catch(err => {
+        console.log('Error loading movie details', err);
+      });
+      
+    });
+    searchResultsContainer.style.display = 'grid';
+  } else {
+    errorMessageNode.innerHTML = 'There is no favourite movie saved';
+  }
+});
+
+// Function to generate and display favorite movie results
+const generateFavouriteMovie = (res) => {
+
+  const movie = res.data;
+  const divTag = document.createElement('div');
+  divTag.className = 'result';
+  divTag.style.width = '210px';
+  // Create the image element for the movie poster
+  const imgTag = document.createElement('img');
+  imgTag.className = 'movie-poster';
+  imgTag.src = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
+  imgTag.alt = movie.title;
+  divTag.appendChild(imgTag);
+
+  // Create the element to display movie title and year
+  const titleTag = document.createElement('h3');
+  titleTag.innerHTML = `${movie.title} (${movie.release_date.substring(0, 4)})`;
+  divTag.appendChild(titleTag);
+
+  searchResultsContainer.appendChild(divTag);
+
+  divTag.addEventListener('click', function () {
+    errorMessageNode.innerHTML = '';
+    searchResultsContainer.style.display = 'none';
+    showMovieDetails(movie.id);
+  });
+};
+
+// Load the search results from the input text entered by user
+const loadSearchResults = (movieName) => {
+  // Make an AJAX request to get a list movies from the user input text
+  axios.get(TMDB_MOVIE_SEARCH_URL, {
+    params: {
+      api_key: TMDB_API_KEY,
+      query: movieName
+    }
+  })
+  .then( res => {
+    generateSearchResults(res);
+  })
+  .catch( err => {
+    console.log( 'Error loading search results', err );
+  });
+};
+
+// Show a list of movies related to the movie name entered
+const generateSearchResults = (res) => {
+  // Clear previous search results
+  searchResultsContainer.replaceChildren();
+
+  // Make sure movieDetailsContainer and castDetailsContainer are hidden
+  movieDetailsContainer.style.display = 'none';
+  castDetailsContainer.style.display = 'none';
+
+  // Loop over each movie result
+  res.data.results.forEach( movie => {
+    const divTag = document.createElement('div');
+    divTag.className = 'result';
+    divTag.style.width = '210px';
+    // Only display movies that has a vote count and has a poster path
+    if(movie.vote_count > 0 && movie.poster_path !== null) {
+      // Create the image element for the movie poster
+      const imgTag = document.createElement('img');
+      imgTag.className = "movie-poster";
+      imgTag.src = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
+      imgTag.alt = movie.title;
+      divTag.appendChild(imgTag);
+
+      // Create the element to display movie title and year only
+      const titleTag = document.createElement('h3');
+      titleTag.innerHTML = `${movie.title} (${movie.release_date.substring(0, 4)})`;
+      divTag.appendChild(titleTag);
+      
+      searchResultsContainer.appendChild(divTag);
+      searchResultsContainer.style.display = 'grid';
+            
+      // Add a click event listener to show movie details when clicked
+      divTag.addEventListener('click', function() {
+        errorMessageNode.innerHTML = '';
+        searchResultsContainer.style.display = 'none';
+        showMovieDetails(movie.id);
+      });
+    } 
+  });
+};
+
+// Function to get movie details based on the movie id
+const showMovieDetails = (movieId) => {
+  // Make an AJAX request to get the details of a selected movie Id
+  axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
+    params: {
+      api_key: TMDB_API_KEY
+    }
+  })
+  .then(res => {
+    generateMovieDetails(res);
+
+    showProductionCompanies(res);
+
+    showCastAndCrew(movieId);
+
+    generateButtons(movieId);
+  })
+  .catch(err => {
+    console.log('Error loading movie details', err);
+  });
+};
+
+// Function to format number as dollar amount with commas
+const formatNumber = (number) => {
+  return number.toLocaleString();
+};
+
+// Function to show the movie details
+const generateMovieDetails = (res) => {
+  let backdropPicture = res.data.backdrop_path;
+  let width = 500;
+  // If there's no backdrop picture, use the poster image instead
+  if(backdropPicture === null) {
+    backdropPicture = res.data.poster_path;
+    width = 300;
+  }
+  // movieDetails will include Official website if there's any
+  const movieDetails = `
+    <div>
+      <h2>${res.data.title} (${res.data.release_date.substring(0, 4)})</h2>
+      <img src="https://image.tmdb.org/t/p/w${width}${backdropPicture}" alt="${res.data.title}">
+      <p>${res.data.overview}</p>
+      <p>Budget: $${formatNumber(res.data.budget)}</p>
+      <p>Revenue: $${formatNumber(res.data.revenue)}</p>
+      <p>Vote Average: ${res.data.vote_average}</p>
+      <p>Vote Count: ${res.data.vote_count}</p>
+      ${res.data.homepage !== '' ? `<p><a href="${res.data.homepage}" target="_blank">Official Website</a></p>` : ''}
+    </div>
+  `;
+  
+  movieDetailsContainer.innerHTML = movieDetails;
+  movieDetailsContainer.style.display = 'block';
+};
+
+// Function to show 'Add to Favourite' and 'Remove from Favourite' buttons
+const generateButtons = (id) => {
+  const buttonContainer = document.createElement('div');
+  buttonContainer.style.padding = '20px';
+
+  // Check if the movie is already in the favorites list
+  const isFavourite = favouriteMovieIDs.includes(id);
+
+  const favouriteButton = document.createElement('button');
+  favouriteButton.style.fontSize = '20px';
+  favouriteButton.innerHTML = isFavourite ? 'Remove from Favourite' : 'Add to Favourite';
+  favouriteButton.style.marginRight = '20px';
+  buttonContainer.appendChild(favouriteButton);
+
+  const backToSearchResultsButton = document.createElement('button');
+  backToSearchResultsButton.style.fontSize = '20px';
+  backToSearchResultsButton.innerHTML = 'Back to Search Results';
+  buttonContainer.appendChild(backToSearchResultsButton);
+
+  // For some reason, if I didn't delay, the button doesn't appear on the bottom
+  setTimeout(() => {
+    movieDetailsContainer.appendChild(buttonContainer);
+  }, 500);
+
+  // Toggle the favorite status when the button is clicked
+  favouriteButton.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    if (isFavourite) {
+      // Remove the movie from the favorites list
+      const index = favouriteMovieIDs.indexOf(id);
+      if (index !== -1) {
+        favouriteMovieIDs.splice(index, 1);
+        saveMoviesToLocalStorage();
+      }
+    } else {
+      // Add the movie to the favorites list
+      favouriteMovieIDs.push(id);
+      saveMoviesToLocalStorage();
+    }
+    // Toggle the button text
+    favouriteButton.innerHTML = isFavourite ? 'Add to Favourite' : 'Remove from Favourite';
+  });
+
+  // Handle the back button to return to the search results
+  backToSearchResultsButton.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    movieDetailsContainer.style.display = 'none';
+    searchResultsContainer.style.display = 'grid';
+  });
+};
+
+// Save favourite movies to local storage with 'favourites' as key name
+const saveMoviesToLocalStorage = function(){
+  localStorage.setItem('favourites', JSON.stringify(favouriteMovieIDs));
+};
+
+
+// Function to show production companies
+const showProductionCompanies = (res) => {
+  const productionCompanies = res.data.production_companies;
+  if (productionCompanies.length > 0) {
+    const productionCompaniesList = document.createElement('ul');
+    productionCompaniesList.innerHTML = "<h3>Production Companies:</h3>";
+    productionCompaniesList.style.textAlign = 'left';
+    movieDetailsContainer.appendChild(productionCompaniesList);
+
+    productionCompanies.forEach(company => {
+      const companyItem = document.createElement('li');
+      const companyLink = document.createElement('a');
+      companyLink.href = `https://www.themoviedb.org/company/${company.id}`;
+      companyLink.target = '_blank';
+      companyLink.innerText = company.name;
+      companyItem.appendChild(companyLink);
+      productionCompaniesList.appendChild(companyItem);
+    });
+  }
+};
+
+// Funtion to get cast and crew names based on the movie id
+const showCastAndCrew = (movieId) => {
+  // Make an AJAX request to get the detailed cast & crew information
+  axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
+    params: {
+      api_key : TMDB_API_KEY
+    }
+  })
+  .then(res => {
+    generateCastandCrew(res);
+
+    handleCastLinks();
+  })
+  .catch(err => {
+    console.log('Error loading cast & crew information', err);
+  });
+};
+
+// Function to show casts and crews of the movie
+const generateCastandCrew = (res) => {
+  // Create a container div for cast and crew information
+  const castAndCrewSection = document.createElement('div');
+  castAndCrewSection.style.display = 'grid'; // Set the grid display
+  castAndCrewSection.style.gridTemplateColumns = '50% 50%'; // Two columns grid
+  castAndCrewSection.style.textAlign = 'center'; // Center-align the content
+  movieDetailsContainer.appendChild(castAndCrewSection);
+
+  // Create a div for "Cast:" and align it to the left
+  const castDiv = document.createElement('div');
+  castDiv.style.textAlign = 'center';
+  castDiv.innerHTML = "<strong><u>Cast:</u></strong><br>";
+  res.data.cast.slice(0, 5).forEach((cast) => {
+    const castItem = document.createElement('div');
+    const castLink = document.createElement('a');
+    castLink.classList.add('cast-movie-link');
+    castLink.href = '#'; // Set the link's href attribute to "#" (to avoid navigation)
+    castLink.dataset.castId = cast.id; // Set the data-movie-id attribute to the cast ID
+    castLink.innerHTML = cast.name;
+    castItem.appendChild(castLink);
+    castDiv.appendChild(castItem);
+  });
+  castAndCrewSection.appendChild(castDiv);
+
+  // Create a div for "Crew:" and align it to the right
+  const crewDiv = document.createElement('div');
+  crewDiv.style.textAlign = 'center';
+  crewDiv.innerHTML = "<strong><u>Crew:<u/></strong><br>";
+  res.data.crew.slice(0, 5).forEach(crew => {
+    const crewItem = document.createElement('div');
+    crewItem.innerHTML = `${crew.job}: ${crew.name}`;
+    crewDiv.appendChild(crewItem);
+  });
+  castAndCrewSection.appendChild(crewDiv);
+};
+
+// Add the click event listener to the cast name links
+const handleCastLinks = () => {
+  const castLinks = document.querySelectorAll('.cast-movie-link');
+  castLinks.forEach((link) => {
+    link.addEventListener('click', function (ev) {
+      ev.preventDefault();
+      movieDetailsContainer.style.display = 'none';
+      showCastDetails(this.dataset.castId);
+    });
+  });
+};
+
+// Function to show the details page for a cast member
+const showCastDetails = function(castId) {
+  // Make an AJAX request to get the detailed information about the cast member
+  axios.get(`https://api.themoviedb.org/3/person/${castId}`, {
+    params: {
+      api_key : TMDB_API_KEY
+    }
+  })
+  .then(res => {
+    // Format the retrieved data into HTML tags for the cast member details
+    const castDetails = `
+      <div>
+        <h2>${res.data.name}</h2>
+        <img src="https://image.tmdb.org/t/p/w500${res.data.profile_path}" alt="${res.data.name}">
+        <p>Birthday: ${res.data.birthday}</p>
+        <p>Place of Birth: ${res.data.place_of_birth}</p>
+      </div>
+    `;
+
+    castDetailsContainer.innerHTML = castDetails;
+    castDetailsContainer.style.display = 'block';
+    generateBackToMovieButton();
+  })
+  .catch(err => {
+    console.log('Error loading cast member details', err);
+  });
+};
+
+// Create 'Back to movie details' button in cast details page
+const generateBackToMovieButton = () => {
+  const buttonContainer = document.createElement('div');
+  const backToMovieButton = document.createElement('button');
+  backToMovieButton.style.fontSize = '20px';
+  buttonContainer.appendChild(backToMovieButton);
+  backToMovieButton.innerHTML = 'Back to Movie Details';
+  buttonContainer.style.padding = '20px';
+  castDetailsContainer.appendChild(buttonContainer);;
+
+  backToMovieButton.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    castDetailsContainer.style.display = 'none';
+    movieDetailsContainer.style.display = 'block';
+  });
+};
+
+
+
